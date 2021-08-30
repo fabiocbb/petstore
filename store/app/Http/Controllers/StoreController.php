388368b5f8b5d0c086e7b11c4ea997b8a8cbab59
Fabio Gagno <9637529+fabiocbb@users.noreply.github.com>
2021-08-30 22:21:42 +1000

    Add files via upload
    store microservice

---
+++ store/app/Http/Controllers/StoreController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class StoreController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get inventory by status.
+     *
+     * @return void
+     */
+    public function getInventoryByStatus(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Create a new order.
+     *
+     * @return void
+     */
+    public function createOrder(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Get order via id.
+     *
+     * @return void
+     */
+    public function getOrderById($petId)
+    {
+        //
+    }
+
+    /**
+     * Delete order via id.
+     *
+     * @return void
+     */
+    public function delete($petId)
+    {
+        //
+    }
+}
